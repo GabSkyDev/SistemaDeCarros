6d0cfa6dbda24f74dcf3b685150eee792e0af62a
Gabriel Sousa <ghostamino08@gmail.com>
2025-03-18 23:46:10 -0300

    primeiro commit

---
+++ src/test/java/dev/java/SistemaCarros/SistemaCarrosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java.SistemaCarros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCarrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
